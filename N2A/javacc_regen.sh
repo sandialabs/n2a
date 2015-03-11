a9e3b2b5d86e30b1b1c626191c003a1a330fe7d1
frothga <frothga@gmail.com>
2015-03-11 15:48:56 -0600

    Update parser.

--- N2A/javacc_regen.sh
+++ N2A/javacc_regen.sh
@@ -2,14 +2,6 @@
 
 export PROJ_ROOT=`pwd`
 
-# Purge generated files that might be replaced
-cd "$PROJ_ROOT/src/gov/sandia/n2a/parsing/gen"
-rm TokenMgrError.java
-rm ParseException.java
-rm Token.java
-rm JavaCharStream.java
-
-# Generate
-cd "$PROJ_ROOT/src/gov/sandia/n2a/parsing/grammar"
-java -classpath "$PROJ_ROOT/lib/javacc-5.0/javacc.jar" jjtree n2a.jjt
-java -classpath "$PROJ_ROOT/lib/javacc-5.0/javacc.jar" javacc ../gen/n2a.jj
+cd "$PROJ_ROOT/src/gov/sandia/n2a/language/parse"
+java -classpath "$PROJ_ROOT/lib/javacc-6.0/javacc.jar" jjtree grammar.jjt
+java -classpath "$PROJ_ROOT/lib/javacc-6.0/javacc.jar" javacc grammar.jj
