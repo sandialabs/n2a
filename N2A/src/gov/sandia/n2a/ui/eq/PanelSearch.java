166ce78ae60ef1cca0a6b8bfb525624e0e2d042f
frothga <frothga@sandia.gov>
2017-05-18 11:45:04 -0600

    Update copyright notice.

--- N2A/src/gov/sandia/n2a/ui/eq/PanelSearch.java
+++ N2A/src/gov/sandia/n2a/ui/eq/PanelSearch.java
@@ -1,8 +1,7 @@
 /*
-Copyright 2013,2016,2017 Sandia Corporation.
-Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+Copyright 2013,2016,2017 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+Under the terms of Contract DE-NA0003525 with NTESS,
 the U.S. Government retains certain rights in this software.
-Distributed under the BSD-3 license. See the file LICENSE for details.
 */
 
 package gov.sandia.n2a.ui.eq;
--- N2A/src/gov/sandia/n2a/ui/ref/ActivityReference.java
+++ N2A/src/gov/sandia/n2a/ui/ref/ActivityReference.java
@@ -1,9 +1,8 @@
 /*
-Copyright 2017 Sandia Corporation.
-Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+Copyright 2017 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+Under the terms of Contract DE-NA0003525 with NTESS,
 the U.S. Government retains certain rights in this software.
-Distributed under the BSD-3 license. See the file LICENSE for details.
- */
+*/
 
 package gov.sandia.n2a.ui.ref;
 
