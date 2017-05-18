c36c1050c22cc86b0ede4c5070f40661ecf073c0
frothga <frothga@sandia.gov>
2017-05-18 12:07:23 -0600

    Update license text, and embed in About screen.

--- N2A/src/gov/sandia/n2a/ui/settings/SettingsAbout.java
+++ N2A/src/gov/sandia/n2a/ui/settings/SettingsAbout.java
@@ -42,11 +42,28 @@ public class SettingsAbout extends JPanel implements Settings
             "<html><body>" +
             "<p><span style='font-size:150%'>" + pc.get ("name") + "</span> version " + pc.get ("version") + "</p>" +
             "<hr/>" +
-            "<p>&copy;2013,2015-2017 Sandia Corporation. Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain rights in this software.</p>" +
-            "<p>This software is released under the BSD license.  Please refer to the license information provided in this distribution for the complete text.</p>" +
-            "<p>Coders: Fred Rothganger, Derek Trumbo, Christy Warrender, Felix Wang</p>" +
-            "<p>Concept development: Brad Aimone, Corinne Teeter, Steve Verzi, Asmeret Bier, Brandon Rohrer, Ann Speed</p>" +
+            "<p>Coders: Fred Rothganger, Derek Trumbo, Christy Warrender</p>" +
+            "<p>Concept development: Brad Aimone, Corinne Teeter, Steve Verzi, Asmeret Bier, Brandon Rohrer, Ann Speed, Felix Wang</p>" +
             "<p>Technical support: <a href=\"mailto:frothga@sandia.gov\">frothga@sandia.gov</a></p>" +
+            "<hr/>" +
+            "<p>&copy;2013-2017 National Technology & Engineering Solutions of Sandia, LLC (NTESS).</p>" +
+            "<p>Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains certain rights in this software.</p>" +
+            "<p>Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:</p>" +
+            "<ul>" +
+            "<li>Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.</li>" +
+            "<li>Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.</li>" +
+            "<li>Neither the name of NTESS nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.</li>" +
+            "</ul>" +
+            "<p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND" +
+            "ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED" +
+            "WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE" +
+            "DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION BE LIABLE FOR ANY" +
+            "DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES" +
+            "(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;" +
+            "LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND" +
+            "ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT" +
+            "(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS" +
+            "SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p>" +
             "</body></html>"
         );
         html.setEditable (false);
