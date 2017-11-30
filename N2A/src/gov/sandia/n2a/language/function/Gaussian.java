4bfef5d7e42d9ca3f34298e3b2b95e61ac2c8d1f
Fred Rothganger <frothga@sandia.gov>
2017-11-30 07:06:01 -0700

    Change interface to random number generators. They now take scale value which also indicate dimensionality.

--- N2A/src/gov/sandia/n2a/language/function/Gaussian.java
+++ N2A/src/gov/sandia/n2a/language/function/Gaussian.java
@@ -43,25 +43,46 @@ public class Gaussian extends Function
 
     public Type eval (Instance context) throws EvaluationException
     {
-        if (operands.length > 1) throw new EvaluationException ("too many arguments to gaussian()");
-
         Random random;
         Simulator simulator = Simulator.getSimulator (context);
         if (simulator == null) random = new Random ();
         else                   random = simulator.random;
 
-        if (operands.length == 1)
+        if (operands.length == 0) return new Scalar (random.nextGaussian ());
+
+        Type sigma = operands[0].eval (context);
+        if (sigma instanceof Scalar)
         {
-            int dimension = (int) Math.round (((Scalar) operands[0].eval (context)).value);
-            if (dimension > 1)
+            return new Scalar (random.nextGaussian () * ((Scalar) sigma).value);
+        }
+        else if (sigma instanceof Matrix)
+        {
+            Matrix scale = (Matrix) sigma;
+            int rows    = scale.rows ();
+            int columns = scale.columns ();
+            if (columns == 1)
             {
-                Matrix result = new MatrixDense (dimension, 1);
-                for (int i = 0; i < dimension; i++) result.set (i, random.nextGaussian ());
+                Matrix result = new MatrixDense (rows, 1);
+                for (int i = 0; i < rows; i++) result.set (i, random.nextGaussian () * scale.get (i, 0));
                 return result;
             }
+            else if (rows == 1)
+            {
+                Matrix result = new MatrixDense (columns, 1);
+                for (int i = 0; i < columns; i++) result.set (i, random.nextGaussian () * scale.get (0, i));
+                return result;
+            }
+            else
+            {
+                Matrix temp = new MatrixDense (columns, 1);
+                for (int i = 0; i < columns; i++) temp.set (i, random.nextGaussian ());
+                return sigma.multiply (temp);
+            }
         }
-        // operands.length == 0
-        return new Scalar (random.nextGaussian ());
+        else
+        {
+            return new Scalar (random.nextGaussian ());  // We could throw an exception, but this is easy enough.
+        }
     }
 
     public String toString ()
--- N2A/src/gov/sandia/n2a/language/function/Uniform.java
+++ N2A/src/gov/sandia/n2a/language/function/Uniform.java
@@ -43,25 +43,46 @@ public class Uniform extends Function
 
     public Type eval (Instance context) throws EvaluationException
     {
-        if (operands.length > 1) throw new EvaluationException ("too many arguments to gaussian()");
-
         Random random;
         Simulator simulator = Simulator.getSimulator (context);
         if (simulator == null) random = new Random ();
         else                   random = simulator.random;
 
-        if (operands.length == 1)
+        if (operands.length == 0) return new Scalar (random.nextDouble ());
+
+        Type sigma = operands[0].eval (context);
+        if (sigma instanceof Scalar)
         {
-            int dimension = (int) Math.round (((Scalar) operands[0].eval (context)).value);
-            if (dimension > 1)
+            return new Scalar (random.nextDouble () * ((Scalar) sigma).value);
+        }
+        else if (sigma instanceof Matrix)
+        {
+            Matrix scale = (Matrix) sigma;
+            int rows    = scale.rows ();
+            int columns = scale.columns ();
+            if (columns == 1)
             {
-                Matrix result = new MatrixDense (dimension, 1);
-                for (int i = 0; i < dimension; i++) result.set (i, random.nextDouble ());
+                Matrix result = new MatrixDense (rows, 1);
+                for (int i = 0; i < rows; i++) result.set (i, random.nextDouble () * scale.get (i, 0));
                 return result;
             }
+            else if (rows == 1)
+            {
+                Matrix result = new MatrixDense (columns, 1);
+                for (int i = 0; i < columns; i++) result.set (i, random.nextDouble () * scale.get (0, i));
+                return result;
+            }
+            else
+            {
+                Matrix temp = new MatrixDense (columns, 1);
+                for (int i = 0; i < columns; i++) temp.set (i, random.nextDouble ());
+                return sigma.multiply (temp);
+            }
         }
-        // operands.length == 0
-        return new Scalar (random.nextDouble ());
+        else
+        {
+            return new Scalar (random.nextDouble ());  // We could throw an exception, but this is easy enough.
+        }
     }
 
     public String toString ()
