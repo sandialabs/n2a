5487a36755a2bac5474b7bb3a097f7326c4a4bdf
frothga <frothga@sandia.gov>
2018-09-11 11:18:56 -0600

    C: Arrange RendererC operators in purely alphabetical order

--- N2A/src/gov/sandia/n2a/backend/c/RendererC.java
+++ N2A/src/gov/sandia/n2a/backend/c/RendererC.java
@@ -64,15 +64,6 @@ public class RendererC extends Renderer
             }
             return false;
         }
-        if (op instanceof AccessVariable)
-        {
-            AccessVariable av = (AccessVariable) op;
-            int shift = av.exponent - av.exponentNext;
-            if (useExponent  &&  shift != 0) result.append ("(");
-            result.append (job.resolve (av.reference, this, false));
-            if (useExponent  &&  shift != 0) result.append (printShift (shift) + ")");
-            return true;
-        }
         if (op instanceof AccessElement)
         {
             AccessElement ae = (AccessElement) op;
@@ -93,9 +84,47 @@ public class RendererC extends Renderer
             }
             return true;
         }
+        if (op instanceof AccessVariable)
+        {
+            AccessVariable av = (AccessVariable) op;
+            int shift = av.exponent - av.exponentNext;
+            if (useExponent  &&  shift != 0) result.append ("(");
+            result.append (job.resolve (av.reference, this, false));
+            if (useExponent  &&  shift != 0) result.append (printShift (shift) + ")");
+            return true;
+        }
         if (op instanceof BuildMatrix)
         {
             result.append (job.matrixNames.get (op));
+            return true;
+        }
+        if (op instanceof Constant)
+        {
+            Constant c = (Constant) op;
+            Type o = c.value;
+            if (o instanceof Scalar)
+            {
+                result.append (print (((Scalar) o).value, c.exponentNext));
+                return true;
+            }
+            if (o instanceof Text)
+            {
+                result.append ("\"" + o.toString () + "\"");
+                return true;
+            }
+            if (o instanceof Matrix)
+            {
+                result.append (job.matrixNames.get (op));
+                return true;
+            }
+            return false;
+        }
+        if (op instanceof Event)
+        {
+            Event e = (Event) op;
+            // The cast to bool gets rid of the specific numeric value from flags.
+            // If used in a numeric expression, it should convert to either 1 or 0.
+            result.append ("((bool) (flags & (" + bed.localFlagType + ") 0x1 << " + e.eventType.valueIndex + "))");
             return true;
         }
         if (op instanceof Exp)
@@ -106,6 +135,85 @@ public class RendererC extends Renderer
             a.render (this);
             if (useExponent) result.append (", " + e.exponentNext);
             result.append (")");
+            return true;
+        }
+        if (op instanceof Gaussian)
+        {
+            Gaussian g = (Gaussian) op;
+            int shift = op.exponent - op.exponentNext;
+            if (useExponent  &&  shift != 0) result.append ("(");
+            result.append ("gaussian<" + job.T + "> (");
+            if (g.operands.length > 0)
+            {
+                g.operands[0].render (this);
+            }
+            result.append (")");
+            if (useExponent  &&  shift != 0) result.append (printShift (shift) + ")");
+            return true;
+        }
+        if (op instanceof Grid)
+        {
+            Grid g = (Grid) op;
+            boolean raw = g.operands.length >= 5  &&  g.operands[4].getString ().contains ("raw");
+
+            result.append ("grid");
+            if (raw) result.append ("Raw");
+            result.append ("<" + job.T + "> (");
+
+            int count = Math.min (4, g.operands.length);
+            if (count > 0) g.operands[0].render (this);
+            for (int i = 1; i < count; i++)
+            {
+                result.append (", ");
+                g.operands[i].render (this);
+            }
+
+            result.append (")");
+            return true;
+        }
+        if (op instanceof Input)
+        {
+            Input i = (Input) op;
+            String inputName;
+            if (i.operands[0] instanceof Constant) inputName = job.inputNames.get (i.operands[0].toString ());
+            else                                   inputName = job.inputNames.get (i);
+
+            String mode = "";
+            if (i.operands.length > 3)
+            {
+                mode = i.operands[3].toString ();  // just assuming it's a constant string
+            }
+            else if (i.operands[1] instanceof Constant)
+            {
+                Constant c = (Constant) i.operands[1];
+                if (c.value instanceof Text) mode = c.toString ();
+            }
+
+            int shift = i.exponent - i.exponentNext;
+            if (useExponent  &&  shift != 0) result.append ("(");
+            if (mode.contains ("columns"))
+            {
+                result.append (inputName + "->getColumns ()");
+            }
+            else
+            {
+                Operator op1 = i.operands[1];
+                Operator op2 = i.operands[2];
+                result.append (inputName + "->get");
+                if (   mode.contains ("raw")   // select raw mode, but only if column is not identified by a string
+                    && ! job.stringNames.containsKey (op2)
+                    && ! (op2 instanceof Constant  &&  ((Constant) op2).value instanceof Text))
+                {
+                    result.append ("Raw");
+                }
+                result.append (" (");
+                op1.render (this);
+                result.append (", ");
+                op2.render (this);
+                result.append (")");
+            }
+            if (useExponent  &&  shift != 0) result.append (printShift (shift) + ")");
+
             return true;
         }
         if (op instanceof Log)
@@ -129,6 +237,31 @@ public class RendererC extends Renderer
             result.append (")");
             return true;
         }
+        if (op instanceof Output)
+        {
+            Output o = (Output) op;
+            String outputName;
+            if (o.operands[0] instanceof Constant) outputName = job.outputNames.get (o.operands[0].toString ());
+            else                                   outputName = job.outputNames.get (o);
+            result.append (outputName + "->trace (Simulator<" + job.T + ">::instance.currentEvent->t, ");
+
+            if (o.operands.length > 2)  // column name is explicit
+            {
+                o.operands[2].render (this);
+            }
+            else  // column name is generated, so use prepared string value
+            {
+                String stringName = job.stringNames.get (op);  // generated column name is associated with Output function itself, rather than one of its operands
+                result.append (stringName);
+            }
+            result.append (", ");
+
+            o.operands[1].render (this);
+            if (useExponent) result.append (", " + o.operands[1].exponentNext);
+            result.append (")");
+
+            return true;
+        }
         if (op instanceof Power)
         {
             Power p = (Power) op;
@@ -139,81 +272,6 @@ public class RendererC extends Renderer
             result.append (", ");
             b.render (this);
             if (useExponent) result.append (", " + a.exponentNext + ", " + p.exponentNext);
-            result.append (")");
-            return true;
-        }
-        if (op instanceof SquareRoot)
-        {
-            SquareRoot s = (SquareRoot) op;
-            Operator a = s.operands[0];
-            result.append ("sqrt (");
-            a.render (this);
-            if (useExponent) result.append (", " + a.exponentNext + ", " + s.exponentNext + ")");
-            return true;
-        }
-        if (op instanceof Tangent)
-        {
-            Tangent t = (Tangent) op;
-            Operator a = t.operands[0];
-            result.append ("tan (");
-            a.render (this);
-            if (useExponent) result.append (", " + a.exponentNext + ", " + t.exponentNext);
-            result.append (")");
-            return true;
-        }
-        if (op instanceof Gaussian)
-        {
-            Gaussian g = (Gaussian) op;
-            int shift = op.exponent - op.exponentNext;
-            if (useExponent  &&  shift != 0) result.append ("(");
-            result.append ("gaussian<" + job.T + "> (");
-            if (g.operands.length > 0)
-            {
-                g.operands[0].render (this);
-            }
-            result.append (")");
-            if (useExponent  &&  shift != 0) result.append (printShift (shift) + ")");
-            return true;
-        }
-        if (op instanceof Uniform)
-        {
-            Uniform u = (Uniform) op;
-            int shift = op.exponent - op.exponentNext;
-            if (useExponent  &&  shift != 0) result.append ("(");
-            result.append ("uniform<" + job.T + "> (");
-            if (u.operands.length > 0)
-            {
-                u.operands[0].render (this);
-            }
-            result.append (")");
-            if (useExponent  &&  shift != 0) result.append (printShift (shift) + ")");
-            return true;
-        }
-        if (op instanceof Event)
-        {
-            Event e = (Event) op;
-            // The cast to bool gets rid of the specific numeric value from flags.
-            // If used in a numeric expression, it should convert to either 1 or 0.
-            result.append ("((bool) (flags & (" + bed.localFlagType + ") 0x1 << " + e.eventType.valueIndex + "))");
-            return true;
-        }
-        if (op instanceof Grid)
-        {
-            Grid g = (Grid) op;
-            boolean raw = g.operands.length >= 5  &&  g.operands[4].getString ().contains ("raw");
-
-            result.append ("grid");
-            if (raw) result.append ("Raw");
-            result.append ("<" + job.T + "> (");
-
-            int count = Math.min (4, g.operands.length);
-            if (count > 0) g.operands[0].render (this);
-            for (int i = 1; i < count; i++)
-            {
-                result.append (", ");
-                g.operands[i].render (this);
-            }
-
             result.append (")");
             return true;
         }
@@ -263,96 +321,38 @@ public class RendererC extends Renderer
 
             return true;
         }
-        if (op instanceof Output)
+        if (op instanceof SquareRoot)
         {
-            Output o = (Output) op;
-            String outputName;
-            if (o.operands[0] instanceof Constant) outputName = job.outputNames.get (o.operands[0].toString ());
-            else                                   outputName = job.outputNames.get (o);
-            result.append (outputName + "->trace (Simulator<" + job.T + ">::instance.currentEvent->t, ");
-
-            if (o.operands.length > 2)  // column name is explicit
-            {
-                o.operands[2].render (this);
-            }
-            else  // column name is generated, so use prepared string value
-            {
-                String stringName = job.stringNames.get (op);  // generated column name is associated with Output function itself, rather than one of its operands
-                result.append (stringName);
-            }
-            result.append (", ");
-
-            o.operands[1].render (this);
-            if (useExponent) result.append (", " + o.operands[1].exponentNext);
+            SquareRoot s = (SquareRoot) op;
+            Operator a = s.operands[0];
+            result.append ("sqrt (");
+            a.render (this);
+            if (useExponent) result.append (", " + a.exponentNext + ", " + s.exponentNext + ")");
+            return true;
+        }
+        if (op instanceof Tangent)
+        {
+            Tangent t = (Tangent) op;
+            Operator a = t.operands[0];
+            result.append ("tan (");
+            a.render (this);
+            if (useExponent) result.append (", " + a.exponentNext + ", " + t.exponentNext);
             result.append (")");
-
             return true;
         }
-        if (op instanceof Input)
+        if (op instanceof Uniform)
         {
-            Input i = (Input) op;
-            String inputName;
-            if (i.operands[0] instanceof Constant) inputName = job.inputNames.get (i.operands[0].toString ());
-            else                                   inputName = job.inputNames.get (i);
-
-            String mode = "";
-            if (i.operands.length > 3)
-            {
-                mode = i.operands[3].toString ();  // just assuming it's a constant string
-            }
-            else if (i.operands[1] instanceof Constant)
-            {
-                Constant c = (Constant) i.operands[1];
-                if (c.value instanceof Text) mode = c.toString ();
-            }
-
-            int shift = i.exponent - i.exponentNext;
+            Uniform u = (Uniform) op;
+            int shift = op.exponent - op.exponentNext;
             if (useExponent  &&  shift != 0) result.append ("(");
-            if (mode.contains ("columns"))
+            result.append ("uniform<" + job.T + "> (");
+            if (u.operands.length > 0)
             {
-                result.append (inputName + "->getColumns ()");
+                u.operands[0].render (this);
             }
-            else
-            {
-                Operator op1 = i.operands[1];
-                Operator op2 = i.operands[2];
-                result.append (inputName + "->get");
-                if (   mode.contains ("raw")   // select raw mode, but only if column is not identified by a string
-                    && ! job.stringNames.containsKey (op2)
-                    && ! (op2 instanceof Constant  &&  ((Constant) op2).value instanceof Text))
-                {
-                    result.append ("Raw");
-                }
-                result.append (" (");
-                op1.render (this);
-                result.append (", ");
-                op2.render (this);
-                result.append (")");
-            }
+            result.append (")");
             if (useExponent  &&  shift != 0) result.append (printShift (shift) + ")");
-
             return true;
-        }
-        if (op instanceof Constant)
-        {
-            Constant c = (Constant) op;
-            Type o = c.value;
-            if (o instanceof Scalar)
-            {
-                result.append (print (((Scalar) o).value, c.exponentNext));
-                return true;
-            }
-            if (o instanceof Text)
-            {
-                result.append ("\"" + o.toString () + "\"");
-                return true;
-            }
-            if (o instanceof Matrix)
-            {
-                result.append (job.matrixNames.get (op));
-                return true;
-            }
-            return false;
         }
         return false;
     }
