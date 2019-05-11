fe257b6c32efed6e42e3b69b57b434bf06244cdf
Fred Rothganger <frothga@sandia.gov>
2019-05-11 07:23:42 -0600

    UI: better positioning of connection labels

--- N2A/src/gov/sandia/n2a/ui/eq/GraphEdge.java
+++ N2A/src/gov/sandia/n2a/ui/eq/GraphEdge.java
@@ -194,13 +194,9 @@ public class GraphEdge
             Vector2 w2 = c.add (ba.multiply (length));
             shape = new CubicCurve2D.Double (tip.x, tip.y, w1.x, w1.y, w2.x, w2.y, c.x, c.y);
 
-            // TODO: find intersection between cubic curve and edges of box
-            // See code at https://www.particleincell.com/2013/cubic-line-intersection
-            // The code here just intersects a straight segment to the first control point,
-            // which can be a very bad approximation.
-            double Clength = Cbounds.getWidth () + Cbounds.getHeight ();
-            root = c.add (ba.multiply (Clength));  // guaranteed to be outside of c
-            root = intersection (new Segment2 (c, root), Cbounds);  // on boundary of c
+            Spline spline = new Spline ((CubicCurve2D) shape);
+            root = intersection (spline, Cbounds);  // on boundary of c
+            if (root == null) root = new Vector2 (100, 100);
         }
 
         // Arrow head
@@ -290,10 +286,10 @@ public class GraphEdge
     }
 
     /**
-        Finds the point on the edge of the given rectangle nearest to the given point.
-        Returns null if the given point is inside the rectangle.
+        Finds the point on the edge of the given rectangle that intersects with the given line segment.
+        Returns null if the segment does not cross the perimeter of the rectangle.
     **/
-    public static Vector2 intersection (Segment2 s0, Rectangle b)
+    public static Vector2 intersection (Segment2 s, Rectangle b)
     {
         double x0 = b.getMinX ();
         double y0 = b.getMinY ();
@@ -303,28 +299,72 @@ public class GraphEdge
         // left edge
         Vector2 p0 = new Vector2 (x0, y0);
         Vector2 p1 = new Vector2 (x0, y1);
-        double t = s0.intersection (new Segment2 (p0, p1));
+        double t = s.intersection (new Segment2 (p0, p1));
 
         // bottom edge
         p0.x = p1.x;
         p0.y = p1.y;
         p1.x = x1;
-        t = Math.min (t, s0.intersection (new Segment2 (p0, p1)));
+        t = Math.min (t, s.intersection (new Segment2 (p0, p1)));
 
         // right edge
         p0.x = p1.x;
         p0.y = p1.y;
         p1.y = y0;
-        t = Math.min (t, s0.intersection (new Segment2 (p0, p1)));
+        t = Math.min (t, s.intersection (new Segment2 (p0, p1)));
 
         // top edge
         p0.x = p1.x;
         p0.y = p1.y;
         p1.x = x0;
-        t = Math.min (t, s0.intersection (new Segment2 (p0, p1)));
+        t = Math.min (t, s.intersection (new Segment2 (p0, p1)));
 
         if (t > 1) return null;
-        return s0.paramtetricPoint (t);
+        return s.paramtetricPoint (t);
+    }
+
+    /**
+        Finds the point on the edge of the given rectangle that intersects with the given cubic spline.
+        Returns null if the spline does not cross the perimeter of the rectangle.
+    **/
+    public static Vector2 intersection (Spline s, Rectangle b)
+    {
+        double x0 = b.getMinX ();
+        double y0 = b.getMinY ();
+        double x1 = b.getMaxX ();
+        double y1 = b.getMaxY ();
+        System.out.println ("Intersect: " + x0 + " " + y0 + " " + x1 + " " + y1);
+        System.out.println ("  " + s.p0);
+        System.out.println ("  " + s.p1);
+        System.out.println ("  " + s.p2);
+        System.out.println ("  " + s.p3);
+
+        // left edge
+        Vector2 p0 = new Vector2 (x0, y0);
+        Vector2 p1 = new Vector2 (x0, y1);
+        double t = s.intersection (new Segment2 (p0, p1));
+
+        // bottom edge
+        p0.x = p1.x;
+        p0.y = p1.y;
+        p1.x = x1;
+        t = Math.min (t, s.intersection (new Segment2 (p0, p1)));
+
+        // right edge
+        p0.x = p1.x;
+        p0.y = p1.y;
+        p1.y = y0;
+        t = Math.min (t, s.intersection (new Segment2 (p0, p1)));
+
+        // top edge
+        p0.x = p1.x;
+        p0.y = p1.y;
+        p1.x = x0;
+        t = Math.min (t, s.intersection (new Segment2 (p0, p1)));
+
+        System.out.println ("  t = " + t);
+        if (t > 1) return null;
+        return s.paramtetricPoint (t);
     }
 
     public static class Vector2
@@ -472,6 +512,89 @@ public class GraphEdge
         public String toString ()
         {
             return a + "->" + a.add (b);
+        }
+    }
+
+    public static class Spline
+    {
+        Vector2 p0;
+        Vector2 p1;
+        Vector2 p2;
+        Vector2 p3;
+
+        public Spline (CubicCurve2D c)
+        {
+            p0 = new Vector2 (c.getX1     (), c.getY1     ());
+            p1 = new Vector2 (c.getCtrlX1 (), c.getCtrlY1 ());
+            p2 = new Vector2 (c.getCtrlX2 (), c.getCtrlY2 ());
+            p3 = new Vector2 (c.getX2     (), c.getY2     ());
+        }
+
+        public Vector2 paramtetricPoint (double t)
+        {
+            double t1 = 1 - t;
+            double a = t1 * t1 * t1;
+            double b = 3 * t1 * t1 * t;
+            double c = 3 * t1 * t * t;
+            double d = t * t * t;
+            double x = a * p0.x + b * p1.x + c * p2.x + d * p3.x;
+            double y = a * p0.y + b * p1.y + c * p2.y + d * p3.y;
+            return new Vector2 (x, y);
+        }
+
+        /**
+            Finds the point where this segment and that segment intersect.
+            Result is a number in [0,1]. If they do not intersect, result is infinity.
+            Based on https://www.particleincell.com/2013/cubic-line-intersection
+        **/
+        public double intersection (Segment2 that)
+        {
+            // Line formula: Ax + By = C, where [A,B] is a vector perpendicular to the line.
+            // Create perpendicular vector via clockwise rotation.
+            double A =  that.b.y;
+            double B = -that.b.x;
+            double C =  that.a.x * A + that.a.y * B;
+
+            double[] bx = coefficients (p0.x, p1.x, p2.x, p3.x);
+            double[] by = coefficients (p0.y, p1.y, p2.y, p3.y);
+
+            double[] P = new double[4];
+            P[0] = A * bx[0] + B * by[0] - C; // t^0
+            P[1] = A * bx[1] + B * by[1];     // t^1
+            P[2] = A * bx[2] + B * by[2];     // t^2
+            P[3] = A * bx[3] + B * by[3];     // t^3
+            int rootCount = CubicCurve2D.solveCubic (P);
+
+            // verify the roots are in bounds of the linear segment
+            double result = Double.POSITIVE_INFINITY;
+            for (int i = 0; i < rootCount; i++)
+            {
+                double t = P[i];
+                if (t < 0  ||  t > 1) continue;
+
+                // Intersection point on infinite line
+                double x = bx[0] + t * (bx[1] + t * (bx[2] + t * bx[3]));
+                double y = by[0] + t * (by[1] + t * (by[2] + t * by[3]));
+
+                // Convert point to parametric form and check if in range
+                double s = -1;
+                if (Math.abs (that.b.y) > Math.abs (that.b.x)) s = (y - that.a.y) / that.b.y;
+                else if (that.b.x != 0)                        s = (x - that.a.x) / that.b.x;
+                if (s < 0  ||  s > 1) continue;
+                result = Math.min (result, t);
+            }
+
+            return result;
+        }
+
+        public double[] coefficients (double P0, double P1, double P2, double P3)
+        {
+            double[] Z = new double[4];
+            Z[0] =      P0;
+            Z[1] = -3 * P0 + 3 * P1;
+            Z[2] =  3 * P0 - 6 * P1 + 3 * P2;
+            Z[3] =     -P0 + 3 * P1 - 3 * P2 + P3;
+            return Z;
         }
     }
 }
