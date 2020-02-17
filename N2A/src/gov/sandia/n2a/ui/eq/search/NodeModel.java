543e784c42d8e273cb063abd7cb03d7b3504aef8
frothga <frothga@sandia.gov>
2020-02-17 13:35:15 -0700

    UI: more careful retrieval of repo for current model

--- N2A/src/gov/sandia/n2a/ui/eq/search/NodeModel.java
+++ N2A/src/gov/sandia/n2a/ui/eq/search/NodeModel.java
@@ -56,8 +56,9 @@ public class NodeModel extends NodeBase
 
         Color result = null;
         String colorName = "";
+        MNode repo = null;
         MNode mdir = AppData.models.containerFor (key);
-        MNode repo = AppData.repos.child (mdir.key ());  // This can return null if multirepo structure changes and this panel is repainted before the change notification arrives.
+        if (mdir != null) repo = AppData.repos.child (mdir.key ());  // This can return null if multirepo structure changes and this panel is repainted before the change notification arrives.
         if (repo != null) colorName = repo.get ("color");
         if (! colorName.isEmpty ())
         {
