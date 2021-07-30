0d6d0cb5534fb2c42c9c92c477269cc8265ab09e
Fred Rothganger <frothga@sandia.gov>
2021-07-30 09:13:57 -0600

    Mavenize the parser generation

--- N2A/javacc_regen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export PROJ_ROOT=`pwd`
-
-cd "$PROJ_ROOT/src/gov/sandia/n2a/language/parse"
-java -classpath "$PROJ_ROOT/lib/javacc/javacc-7.0.5.jar" jjtree grammar.jjt
-java -classpath "$PROJ_ROOT/lib/javacc/javacc-7.0.5.jar" javacc grammar.jj
