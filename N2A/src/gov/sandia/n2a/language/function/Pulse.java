971e5cf25595332cfb78ef480683d05e4380c33a
Fred Rothganger <frothga@sandia.gov>
2021-09-14 08:07:59 -0600

    Language: more complete implementation of pulse() function

--- N2A/src/gov/sandia/n2a/language/function/Pulse.java
+++ N2A/src/gov/sandia/n2a/language/function/Pulse.java
@@ -6,11 +6,15 @@ the U.S. Government retains certain rights in this software.
 
 package gov.sandia.n2a.language.function;
 
+import javax.measure.Unit;
+
+import gov.sandia.n2a.eqset.EquationSet.ExponentContext;
 import gov.sandia.n2a.language.Function;
 import gov.sandia.n2a.language.Operator;
 import gov.sandia.n2a.language.Type;
 import gov.sandia.n2a.language.type.Instance;
 import gov.sandia.n2a.language.type.Scalar;
+import tech.units.indriya.AbstractUnit;
 
 public class Pulse extends Function
 {
@@ -28,6 +32,54 @@ public class Pulse extends Function
                 return new Pulse ();
             }
         };
+    }
+
+    public void determineExponent (ExponentContext context)
+    {
+        for (int i = 0; i < operands.length; i++)
+        {
+            operands[i].determineExponent (context);
+        }
+        updateExponent (context, 0,  MSB - 2); // Output reaches exactly 1.
+    }
+
+    public void determineExponentNext ()
+    {
+        // Get our working exponent from the time variable.
+        // This assumes that exponent of $t will be imposed on the first operand.
+        Operator op0 = operands[0];
+        op0.exponentNext = op0.exponent;
+        op0.determineExponentNext ();
+
+        for (int i = 1; i < operands.length; i++)
+        {
+            Operator op = operands[i];
+            op.exponentNext = op0.exponent;
+            op.determineExponentNext ();
+        }
+    }
+
+    public void determineUnit (boolean fatal) throws Exception
+    {
+        unit = AbstractUnit.ONE;
+
+        Unit<?> temp = null;
+        for (int i = 0; i < operands.length; i++)
+        {
+            Operator op = operands[i];
+            op.determineUnit (fatal);
+            if (op.unit != null)
+            {
+                if (temp == null  ||  temp.isCompatible (AbstractUnit.ONE))
+                {
+                    temp = op.unit;
+                }
+                else if (fatal  &&  ! op.unit.isCompatible (AbstractUnit.ONE)  &&  ! op.unit.isCompatible (temp))
+                {
+                    throw new Exception (toString () + "(" + temp + " versus " + op.unit + ")");
+                }
+            }
+        }
     }
 
     public Type eval (Instance context)
