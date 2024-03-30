0e64b14384d17aef94474773b8d11b2e0efa36a7
Fred Rothganger <frothga@sandia.gov>
2024-03-30 15:45:29 -0600

    Studies: make OptimizerLM2 the primary LM class

--- N2A/src/gov/sandia/n2a/ui/studies/OptimizerLM2.java
+++ N2A/src/gov/sandia/n2a/ui/studies/OptimizerLM2.java
@@ -6,36 +6,154 @@ the U.S. Government retains certain rights in this software.
 
 package gov.sandia.n2a.ui.studies;
 
+import java.nio.file.Path;
 import java.util.List;
 
+import gov.sandia.n2a.db.AppData;
 import gov.sandia.n2a.db.MNode;
 import gov.sandia.n2a.linear.Factorization;
 import gov.sandia.n2a.linear.FactorizationBK;
 import gov.sandia.n2a.linear.MatrixDense;
+import gov.sandia.n2a.ui.jobs.NodeJob;
+import gov.sandia.n2a.ui.jobs.OutputParser;
+import gov.sandia.n2a.language.UnitValue;
 import gov.sandia.n2a.language.type.Matrix;
 
 /**
-    An extension of OptimizerLM that solves the squared Jacobian (~J*J) rather than J directly.
-    The matrix ~J*J tends to be much more compact than J for large sparse systems, since its
+    This version of Levenberg-Marquardt uses Bunch-Kaufman factorization of the
+    squared Jacobian (~J*J), rather than factoring J directly.
+    See OptimizerLM for history on where this code came from.
+
+    The matrix ~J*J tends to be more compact than J for large sparse systems, since its
     size depends only on the number of variables rather than the number of data points.
-    This also tends to be better conditioned than J. In particular, it can tolerate more
-    zero elements in J and still solve for the next LM step.
+    This also tends to be more tolerant of zeroes in J.
 **/
-public class OptimizerLM2 extends OptimizerLM
+public class OptimizerLM2 extends StudyIterator
 {
+    // Machine epsilon, the difference between 1 and next larger representable value.
+    // We take square root because we want to allow for squaring a number and it still be representable.
+    // Even though we use double for all internal work, we assume only float precision in the
+    // model being optimized.
+    public static final double epsilon = Math.sqrt (Math.ulp (1.0f));
+
+    protected Study       study;
+    protected int         iteration  = -1;
+    protected int         sample;             // This is always relative to one iteration of LM.
+    protected int         baseIndex;          // Overall position in study as of the start of the current iteration. baseIndex+sample should give the current job number.
+    protected int         yindex     = -1;    // Index of the job that provided the current value of y. Used to restore iterator state.
+    protected int         maxIterations;
+    protected int         expectedIterations; // estimate updated after first sample
+    protected List<MNode> loss;
+    protected List<MNode> variables;
+    protected double      toleranceF;
+    protected double      toleranceX;
+    protected double      toleranceG;
+    protected double      perturbation;
+    protected String[]    dummy;
+
+    protected MatrixDense   x;       // current state vector
+    protected MatrixDense   y;       // current value of time series output by model.  y=f(x) where f() is one entire job run.
+    protected MatrixDense   min;     // limits on domain
+    protected MatrixDense   max;
+    protected MatrixDense   J;       // the Jacobian, given current state x
     protected Factorization factorization;
+    protected MatrixDense   p;
+    protected MatrixDense   xp;
+    protected MatrixDense   scales;  // sensitivity of each variable in x
+    protected double        ratio;
+    protected double        par;     // Levenberg-Marquardt parameter (mix between Gauss-Newton and gradient-descent)
+    protected double        xnorm;
+    protected double        ynorm;
+    protected double        pnorm;
+    protected double        delta;   // step bound
 
     public OptimizerLM2 (Study study, List<MNode> loss, List<MNode> variables)
     {
-        super (study, loss, variables);
+        super (loss.get (0).keyPath (study.source.child ("variables")));  // by convention, we store optimizer state under the first loss variable
+        this.study     = study;
+        this.loss      = loss;
+        this.variables = variables;
+
+        maxIterations = study.source.getOrDefault (200,     "config", "maxIterations");
+        toleranceF    = study.source.getOrDefault (epsilon, "config", "toleranceF");
+        toleranceX    = study.source.getOrDefault (epsilon, "config", "toleranceX");
+        toleranceG    = study.source.getOrDefault (epsilon, "config", "toleranceG");
         perturbation  = study.source.getOrDefault (epsilon, "config", "perturbation");
 
-        switch (study.source.getOrDefault ("bk", "config", "factorizer"))
+        int n = variables.size ();
+        x      = new MatrixDense (n, 1);
+        min    = new MatrixDense (n, 1);
+        max    = new MatrixDense (n, 1);
+        p      = new MatrixDense (n, 1);
+        scales = new MatrixDense (n, 1);
+
+        // Determine range limit and initial value for each variable
+        MNode root = study.source.child ("variables");
+        String inherit = study.source.get ("$inherit");
+        MNode model = AppData.docs.childOrEmpty ("models", inherit);
+        for (int c = 0; c < n; c++)
         {
-            case "bk":
-            default:
-                factorization = new FactorizationBK ();
+            MNode v = variables.get (c);
+
+            double vMin = Double.NEGATIVE_INFINITY;
+            double vMax = Double.POSITIVE_INFINITY;
+            String range = v.get ();
+            if (range.startsWith ("["))
+            {
+                range = range.substring (1);
+                range = range.split ("]", 2)[0];
+                String[] pieces = range.split (",");
+                vMin = new UnitValue (pieces[0]).get ();
+                vMax = new UnitValue (pieces[pieces.length - 1]).get ();
+            }
+
+            if (v.child ("min") != null) vMin = new UnitValue (v.get ("min")).get ();
+            if (v.child ("max") != null) vMax = new UnitValue (v.get ("max")).get ();
+            min.set (c, vMin);
+            max.set (c, vMax);
+
+            double value = 0;
+            String initial;
+            if (v.child ("start") == null) initial = model.get (v.keyPath (root));
+            else                           initial = v.get ("start");
+            if (initial.isBlank ())
+            {
+                if (Double.isFinite (vMin)  &&  Double.isFinite (vMax)) value = (vMin + vMax) / 2;
+            }
+            else
+            {
+                value = new UnitValue (initial).get ();
+            }
+            x.set (c, value);
         }
+
+        // Determine dummy variable for loss output() statement
+        // Compare with EquationSet ctor code that handles watch variables.
+        // If "loss" is a state variable (as opposed to temporary), then there will be
+        // a 1-cycle delay, and the first row will be useless.
+        int count = loss.size ();
+        dummy = new String[count];
+        for (int i = 0; i < count; i++)
+        {
+            MNode g = model.child (loss.get (i).keyPath (root));
+            MNode p = g.parent ();
+            String d = "x0";
+            int suffix = 1;
+            while (p.child (d) != null) d = "x" + suffix++;
+            dummy[i] = d;
+        }
+    }
+
+    public int count ()
+    {
+        int result = (variables.size () + 1) * (expectedIterations == 0 ? maxIterations : expectedIterations);
+        if (inner != null) result *= inner.count ();
+        return result;
+    }
+
+    public void restart ()
+    {
+        iteration = -1;
     }
 
     public boolean step ()
@@ -48,6 +166,15 @@ public class OptimizerLM2 extends OptimizerLM
             baseIndex++;     // Always relative to last position of base, especially when coming from a previous optimization run.
             sample    = -1;  // -1 is the base sample. 0 through n-1 are used to construct the Jacobian. sample >= n are for probing optimal step size.
             yindex    = -1;
+            if (factorization == null)
+            {
+                switch (study.source.getOrDefault ("bk", "config", "factorizer"))
+                {
+                    case "bk":
+                    default:
+                        factorization = new FactorizationBK ();
+                }
+            }
             return true;  // Causes one sample (job) to be collected with parameters at the start point. This is the base sample for the first iteration.
             // In subsequent iterations, the base sample will come from the last step of the inner loop.
         }
@@ -360,6 +487,37 @@ public class OptimizerLM2 extends OptimizerLM
         }
     }
 
+    public boolean barrier ()
+    {
+        if (iteration < 0) return false;
+        return sample < 0  ||  sample >= variables.size () - 1;  // At this point, "sample" refers the one most recently started. We want to start blocking right after the last column of the Jacobian is issued.
+    }
+
+    public void save (MNode study)
+    {
+        if (inner != null) inner.save (study);
+        MNode state = node (study);
+
+        state.set (iteration, "iteration");
+        state.set (sample,    "sample");
+        state.set (baseIndex, "baseIndex");
+        state.set (yindex,    "yindex");
+        state.set (ratio,     "ratio");
+        state.set (par,       "par");
+        state.set (delta,     "delta");
+
+        int n = variables.size ();
+        for (int c = 0; c < n; c++)
+        {
+            MNode v = variables.get (c);
+            v.set (x     .get (c), "x");
+            v.set (scales.get (c), "scale");
+            v.set (p     .get (c), "p");
+        }
+
+        // Remaining variables will be recreated from existing data.
+    }
+
     public void load (MNode study)
     {
         if (inner != null) inner.load (study);
@@ -409,5 +567,89 @@ public class OptimizerLM2 extends OptimizerLM
                 J.set (r, c, (series[r] - y.get (r)) / h);
             }
         }
+    }
+
+    public void assign (MNode model)
+    {
+        int n = variables.size ();
+        MNode root = study.source.child ("variables");
+
+        // Output "loss"
+        int count = loss.size ();
+        for (int i = 0; i < count; i++)
+        {
+            MNode l = loss.get (i);
+            MNode g = model.child (l.keyPath (root));
+            MNode p = g.parent ();
+            p.set ("output(\"study\"," + g.key () + ",\"loss" + i + "\")", dummy[i]);
+            // TODO: sampling intervals (see EquationSet ctor)
+        }
+
+        // Set starting point
+        if (sample < 0)
+        {
+            for (int c = 0; c < n; c++)
+            {
+                MNode v = variables.get (c);
+                String[] keyPath = v.keyPath (root);
+                double value = x.get (c);
+                model.set (value, keyPath);
+            }
+            return;
+        }
+
+        // Finite-difference samples to create Jacobian
+        if (sample < n)
+        {
+            // TODO: use structurally-orthogonal cover of Jacobian.
+            // Requires either user-specified structure, or detect structure by observing zeroes in elements of J
+            // for the entire run up to now. If the squared error is over something like a time series, it is
+            // unlikely that any variables are separable. Only when subsets of the output are uniquely connected
+            // to subsets of the input do we get separability. In that case, it is possible to do fewer samples
+            // than the number of variables.
+            for (int c = 0; c < n; c++)
+            {
+                MNode v = variables.get (c);
+                String[] keyPath = v.keyPath (root);
+                double value = x.get (c);
+                if (c == sample)
+                {
+                    double h = perturbation * Math.abs (value);
+                    if (h == 0) h = perturbation;
+                    value += h;
+                    // We don't bound the perturbation. Presumably it is small enough that
+                    // if it transgresses min or max, it does not go so far that the model fails.
+                }
+                model.set (value, keyPath);
+            }
+            return;
+        }
+
+        // sample >= n -- Apply xp, the sample for testing step
+        for (int c = 0; c < n; c++)
+        {
+            MNode v = variables.get (c);
+            String[] keyPath = v.keyPath (root);
+            double value = xp.get (c);
+            model.set (value, keyPath);
+        }
+    }
+
+    public double[] getSeries (int index)
+    {
+        NodeJob node = study.getJob (index);
+        Path jobDir = node.getJobPath ().getParent ();
+        OutputParser parser = new OutputParser ();
+        parser.parse (jobDir.resolve ("study"));
+        int columns = loss.size ();
+        double[] result = new double[parser.rows * columns];
+        for (int i = 0; i < columns; i++)
+        {
+            OutputParser.Column c = parser.getColumn ("loss" + i);
+            int count = c.values.size ();
+            int offset = i * parser.rows + c.startRow;
+            for (int j = 0; j < count; j++) result[offset+j] = c.values.get (j);
+        }
+        return result;
     }
 }
